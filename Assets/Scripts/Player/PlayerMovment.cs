8d8e60cf0fc3b4b1b6c3131d4b96ccd98fbc102b
OrigamiShiro <76973214+OrigamiShiro@users.noreply.github.com>
2022-05-06 22:52:04 +0300

    Update PlayerMovment.cs

--- Assets/Scripts/Player/PlayerMovment.cs
+++ Assets/Scripts/Player/PlayerMovment.cs
@@ -4,82 +4,64 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
-    [SerializeField] private float speed = 10f;
-    private Rigidbody rb;
-    public bool isGrounded;
-    public int boostInpulse = 5000;
-    private float dirX;
+    [SerializeField] private float _speed = 10f;
+    private Rigidbody _rigidbody;
+    public bool IsGrounded;
+    public int BoostInpulse = 5000;
+    private float _directionX;
 
-
-    
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update ()
+    private void Update()
     {
         Jump();
         Boost();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-     { 
-       move();
-     }
-
-     void move() 
-     {
-         rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y, Input.GetAxis("Vertical") * speed);//move
-
-        
-        if (rb.velocity.x < -.01)// flip
-            transform.eulerAngles = new Vector3(0, 180, 0);
-        else if (rb.velocity.x > .01)
-            transform.eulerAngles = new Vector3(0, 0, 0);
-
-     }
-     
-     void Jump()
-   
-     {
-        if (Input.GetButtonDown("Jump") && isGrounded)//jump
-            {
-                isGrounded = false;
-                rb.AddForce(new Vector3(0 , 300 , 0));
-            }
-
-    }
-
-     void OnCollisionEnter()
-     
-    { //ground
-
-         isGrounded = true;
-        
-    }
-
-
-    void Boost ()
-
+    private void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.LeftShift))
-        {
-            speed = 15f;
-        }
-        else 
-        {
-            speed = 10f;
-        }
-
-       dirX = Input.GetAxis("Horizontal") * speed;
-
-    
+        Move();
     }
 
-    
+    private void Move()
+    {
+        _rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * _speed, _rigidbody.velocity.y,
+            Input.GetAxis("Vertical") * _speed); //move
 
-   
+        if (_rigidbody.velocity.x < -.01) // flip
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        else if (_rigidbody.velocity.x > .01)
+            transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded) //jump
+        {
+            IsGrounded = false;
+            _rigidbody.AddForce(new Vector3(0, 300, 0));
+        }
+    }
+
+    private void OnCollisionEnter()
+    {
+        IsGrounded = true;
+    }
+
+    private void Boost()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _speed = 15f;
+        }
+        else
+        {
+            _speed = 10f;
+        }
+
+        _directionX = Input.GetAxis("Horizontal") * _speed;
+    }
 }
