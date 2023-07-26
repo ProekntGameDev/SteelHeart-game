8ea0dfe095db731bf1888c68f256b083a9d8bfe4
van1ove <berezko2004@gmail.com>
2023-07-26 13:36:51 +0300

    definition of parametrs in chase state constructor

--- Assets/Resources/Scripts/AI/Robot/States/RobotState_Chase.cs
+++ Assets/Resources/Scripts/AI/Robot/States/RobotState_Chase.cs
@@ -19,6 +19,8 @@ namespace AI
             _robotVision = robotVision;
             _navMeshAgent = navMeshAgent;
             _speed = speed;
+            _minDistance = minDistance;
+            _maxDistance = maxDistance;
         }
 
         public void OnEnter()
