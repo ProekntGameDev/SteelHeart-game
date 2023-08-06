dd314adc555ec33dfb7f219eda7459bca112bd6f
van1ove <berezko2004@gmail.com>
2023-08-06 18:59:07 +0300

    changes for merge

--- Assets/Resources/Scripts/AI/Robot/States/RobotState_Chase.cs
+++ Assets/Resources/Scripts/AI/Robot/States/RobotState_Chase.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
 namespace AI
 {
     public class RobotState_Chase : IState
@@ -14,46 +13,35 @@ namespace AI
         private float _minDistance;
         private float _maxDistance;
 
-
-        private float _stopDistance;
         public RobotState_Chase(RobotVision robotVision, NavMeshAgent navMeshAgent, float speed, float minDistance, float maxDistance)
         {
             _robotVision = robotVision;
             _navMeshAgent = navMeshAgent;
             _speed = speed;
-            _minDistance = minDistance;
-            _maxDistance = maxDistance;
         }
 
         public void OnEnter()
         {
             _navMeshAgent.speed = _speed;
-
-            _stopDistance = _navMeshAgent.stoppingDistance;
-            
             _navMeshAgent.stoppingDistance = _minDistance;
         }
 
         public void OnExit()
         {
             _player = null;
-            _navMeshAgent.stoppingDistance = _stopDistance;
+            _navMeshAgent.stoppingDistance = 0;
         }
 
         public void Tick()
         {
             if (_robotVision.IsVisible(out _player))
-            {
                 _navMeshAgent.destination = _player.transform.position;
-            }
-                
         }
 
         public bool IsLostPlayer()
         {
             return _player == null || Vector3.Distance(_player.transform.position, _navMeshAgent.transform.position) > _maxDistance;
         }
-
         public bool IsDone()
         {
             return _navMeshAgent.pathStatus == NavMeshPathStatus.PathComplete && _player != null;
