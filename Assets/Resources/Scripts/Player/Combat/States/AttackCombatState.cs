36bacbcfa48a93e6b63f6753e9073a7d7ff90cdd
Overlord <nrbudanov1123@gmail.com>
2023-09-14 17:58:49 +0300

    Combat system improvements & Delete old scripts

--- Assets/Resources/Scripts/Player/Combat/States/AttackCombatState.cs
+++ Assets/Resources/Scripts/Player/Combat/States/AttackCombatState.cs
@@ -1,18 +1,23 @@
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 
 public class AttackCombatState : IState
 {
+    private Player _player;
     private OverlapSphere _attackPoint;
+    private OverlapSphere _punchAssistant;
     private float _damage;
     private float _delay;
     private float _cooldown;
 
     private IEnumerator _attackCoroutine;
 
-    public AttackCombatState(OverlapSphere attackPoint, float delay, float cooldown, float damage)
+    public AttackCombatState(Player player, OverlapSphere attackPoint, OverlapSphere punchAssistant, float delay, float cooldown, float damage)
     {
+        _player = player;
         _attackPoint = attackPoint;
+        _punchAssistant = punchAssistant;
         _delay = delay;
         _cooldown = cooldown;
         _damage = damage;
@@ -36,13 +41,16 @@ public class AttackCombatState : IState
     {
         yield return new WaitForSeconds(_delay);
 
+        Collider bestTarget = _punchAssistant.GetColliders().Where(x => x.TryGetComponent(out IDamagable damagable))
+            .OrderBy(x => Vector3.Distance(x.transform.position, _player.transform.position))
+            .FirstOrDefault();
+
+        if(bestTarget != null)
+            _player.Movement.CharacterController.Rotate((bestTarget.transform.position - _player.transform.position).normalized);
+
         foreach (var collider in _attackPoint.GetColliders())
-        {
             if (collider.TryGetComponent(out IDamagable damagable) && collider.TryGetComponent(out Player player) == false)
-            {
                 damagable.TakeDamage(_damage);
-            }
-        }
 
         yield return new WaitForSeconds(_cooldown);
 
