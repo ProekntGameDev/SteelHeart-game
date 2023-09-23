956432da07222b463afb2a51681262cfb5b8a43c
Overlord <nrbudanov1123@gmail.com>
2023-09-23 17:37:59 +0300

    Bug fix

--- Assets/Resources/Scripts/UI/MainMenuUI.cs
+++ Assets/Resources/Scripts/UI/MainMenuUI.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 public class MainMenuUI : MonoBehaviour
 {
     [Inject] private SceneManager _sceneManager;
+    [Inject] private SaveManager _saveManager;
 
-    public void LoadNext() => _sceneManager.LoadNext();
+    public void NewGame()
+    {
+        if (_saveManager.GetSaves().Length > 0)
+            _saveManager.Delete();
+
+        _sceneManager.LoadNext();
+    }
+
     public void LoadFromSave() => _sceneManager.LoadFromSave();
+
     public void Quit() => _sceneManager.Quit();
 }
